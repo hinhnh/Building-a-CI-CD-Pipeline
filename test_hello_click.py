e3319999e8c19c9431beee706d78b2ac677c8081
hinhnh <hinh.nguyen@myporsperity.com.au>
2022-11-23 16:37:34 +0700

    update files

---
+++ test_hello_click.py
@@ -0,0 +1,7 @@
+from click.testing import CliRunner
+from hello_click import hello
+ 
+def test_hello():
+    runner = CliRunner()
+    result = runner.invoke(hello, ["--name", "Thor", "--color", "blue", "--year", "1990"])
+    assert "Thor" in result.output
