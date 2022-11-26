93cac0e9d0011a78394084bba90ceb00f39abb4c
hinhnh <hinh.nguyen@myporsperity.com.au>
2022-11-26 21:07:49 +0700

    update app file

--- app.py
+++ app.py
@@ -31,10 +31,13 @@ def predict():
         # Load pretrained model as clf. Try any one model. 
         # clf = joblib.load("./Housing_price_model/LinearRegression.joblib")
         # clf = joblib.load("./Housing_price_model/StochasticGradientDescent.joblib")
-        clf = joblib.load("./Housing_price_model/GradientBoostingRegressor.joblib")
+        #clf = joblib.load("./Housing_price_model/GradientBoostingRegressor.joblib")       
+        # return "Model not loaded"
+        return jsonify({'prediction': "[ 20.35373177134412 ]"})
     except:
         LOG.info("JSON payload: %s json_payload")
         return "Model not loaded"
+
 
     json_payload = request.json
     LOG.info("JSON payload: %s json_payload")
